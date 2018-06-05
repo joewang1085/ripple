d941833d6288d530e21418dbd867da5c01176629
joewang1085 <825039075@qq.com>
2018-06-05 17:27:04 +0800

    Create main.go

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"controllers"
+
+	"github.com/ripple"
+)
+
+func main() {
+
+	// Build the REST application
+	app := ripple.NewApplication()
+	// Create a controller and register it. Any number of controllers
+	// can be registered that way.
+	grasperController := controllers.NewGrasperController()
+	app.RegisterController("grasper", grasperController)
+	// Setup the routes. The special patterns `_controller` will automatically match
+	// an existing controller, as defined above. Likewise, `_action` will match any
+	// existing action.
+	app.AddRoute(ripple.Route{Pattern: ":_controller/:_action/:params"})
+	app.AddRoute(ripple.Route{Pattern: ":_controller/:_action"})
+
+	fmt.Println("web listening 127.0.0.1:8881...")
+	http.ListenAndServe(":8881", app)
+}
